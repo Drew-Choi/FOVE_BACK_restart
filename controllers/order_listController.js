6e495d314b6049a70620a4f2fe810e4ac81fd530
Drew <drc920630@gmail.com>
2023-05-20 01:14:46 +0900

    오더리스트 완성 솔트까지 완료

--- controllers/order_listController.js
+++ controllers/order_listController.js
@@ -11,10 +11,11 @@ const getMemberOrderList = async (req, res) => {
     jwt.verify(token, JWT_ACCESS_SECRET, async (err, decoded) => {
       // 토큰 인증 실패
       if (err) return res.status(401).json({ message: '회인인증 실패 로그아웃' });
-
       // 토큰 인증 성공
       const getAllOrderInfo = await Order.find({ user: decoded.id });
-      res.status(200).json(getAllOrderInfo);
+      // 날짜순으로 확실히 정렬 오름차순
+      const array = getAllOrderInfo.sort((a, b) => a.payments.approvedAt - b.payments.approvedAt);
+      res.status(200).json(array);
     });
   } catch (err) {
     console.error(err);
