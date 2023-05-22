fdcf1408b6514373e8273a398c3b72613a49d268
Drew <drc920630@gmail.com>
2023-05-22 20:12:56 +0900

    취소 내역서 불러오는 API구성 완료

--- controllers/order_listController.js
+++ controllers/order_listController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('../mongooseConnect');
 const Order = require('../models/order');
+const Cancle = require('../models/cancel');
 
 const { JWT_ACCESS_SECRET } = process.env;
 
@@ -12,16 +13,17 @@ const changeTimetoNum = (time) => {
   return koreanTime;
 };
 
+// 모든 주문내역서 가져오기
 const getMemberOrderList = async (req, res) => {
   try {
-    const { token } = req.body;
+    const { token } = await req.body;
     // 토큰 받아서 인증 받기
     jwt.verify(token, JWT_ACCESS_SECRET, async (err, decoded) => {
       // 토큰 인증 실패
       if (err) return res.status(401).json({ message: '회인인증 실패 로그아웃' });
       // 토큰 인증 성공
       const getAllOrderInfo = await Order.find({ user: decoded.id });
-      // 날짜순으로 확실히 정렬 오름차순
+      // 날짜순으로 확실히 정렬 내림차순
       const array = getAllOrderInfo.sort(
         (a, b) => changeTimetoNum(b.payments.approvedAt) - changeTimetoNum(a.payments.approvedAt),
       );
@@ -32,6 +34,29 @@ const getMemberOrderList = async (req, res) => {
   }
 };
 
+// 모든 취소내역서 가져오기
+const getCancelList = async (req, res) => {
+  try {
+    const { token } = await req.body;
+    // 토큰 받아 인증 받기
+    jwt.verify(token, JWT_ACCESS_SECRET, async (err, decoded) => {
+      if (err) return res.status(401).json({ message: '회원인증실패' });
+
+      // 토큰 인증 성공시
+      const cancelListInfo = await Cancle.find({ user: decoded.id });
+      // 날짜순으로 확실히 정렬 내림차순
+      const array = cancelListInfo.sort(
+        (a, b) => changeTimetoNum(b.cancels.canceledAt) - changeTimetoNum(a.cancels.canceledAt),
+      );
+      res.status(200).json(array);
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: '알 수 없는 오류' });
+  }
+};
+
+// 취소할 특정 상품 가져오기
 const orderCancelGetItem = async (req, res) => {
   try {
     const { orderId, token } = await req.body;
@@ -49,4 +74,4 @@ const orderCancelGetItem = async (req, res) => {
   }
 };
 
-module.exports = { getMemberOrderList, orderCancelGetItem };
+module.exports = { getMemberOrderList, orderCancelGetItem, getCancelList };
--- routes/order_list.js
+++ routes/order_list.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const { getMemberOrderList, orderCancelGetItem } = require('../controllers/order_listController');
+const { getMemberOrderList, orderCancelGetItem, getCancelList } = require('../controllers/order_listController');
 
 // 모든 주문내역서 가져오기 + 토큰으로 아이디 추출
 router.post('/getMemberOrderList', getMemberOrderList);
+
+// 모든 취내역서 가오기 + 토큰으로 아이디 추출
+router.post('/getCancelList', getCancelList);
 
 // 취소할 특정 주문내역서 호출(토큰과 orderId 두 개 대조하여 확실하게 소환)
 router.post('/getCancelItem', orderCancelGetItem);
