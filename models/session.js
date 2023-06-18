2d34eb9cf102aa77abd2a639b26a7d411890a6f4
Drew <drc920630@gmail.com>
2023-06-19 03:23:19 +0900

    쿠키데이터파스

--- models/session.js
+++ models/session.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const sessionSchema = new mongoose.Schema({
   _id: { type: String },
   expire: { type: Date },
-  session: { type: Object },
+  session: { type: String },
 });
 
 module.exports = mongoose.model('Session', sessionSchema);
