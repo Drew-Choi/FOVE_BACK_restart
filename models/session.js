0e84399083020634ba5e254be1bd1b1a0f1eb07f
Drew <drc920630@gmail.com>
2023-06-24 07:36:47 +0900

    sdsd

--- models/session.js
+++ models/session.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
-const sessionSchema = new mongoose.Schema({});
+const sessionSchema = new mongoose.Schema({
+  _id: { type: String },
+  expire: { type: Date },
+  session: { type: String },
+});
 
 module.exports = mongoose.model('Session', sessionSchema);
