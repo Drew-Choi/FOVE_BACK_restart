974f1a0ea18afc928665ee0b39d5afa95e43d4aa
Drew <drc920630@gmail.com>
2023-06-24 03:21:17 +0900

    sd

--- routes/toss.js
+++ routes/toss.js
@@ -28,7 +28,7 @@ const midlewareCookieParser = cookieParser();
 
 router.get('/approve', sessionMiddleware, tossApprove);
 
-router.get('/data', midlewareCookieParser, sessionMiddleware, paymentData);
+router.get('/data', midlewareCookieParser, paymentData);
 
 router.post('/cancel', tossCancel);
 
