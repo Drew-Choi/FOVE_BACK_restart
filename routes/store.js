64ce0c3448b394ce9979788bdd1cc74ef76947bc
Drew <drc920630@gmail.com>
2023-08-18 15:16:46 +0900

    백엔드 조금 수정

--- routes/store.js
+++ routes/store.js
@@ -15,9 +15,6 @@ router.get('/', getAllProducts); // 전체 상품 데이터 가져오기
 // 전체상품 보기 /store/all
 router.get('/all', getAllProducts); //  전체 상품 데이터 가져오기
 
-// 신상품 보기 /store/new
-router.get('/new', getNewProducts); // 일정 기간 내 등록된 상품 데이터 가져오기
-
 // 카테고리별 상품 보기 /store/"카테고리명"
 router.get('/:category', getProductsByCategory); // 카테고리에 따른 상품 데이터 가져오기
 
